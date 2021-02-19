8845deed7baa630f7bbf76b057385672a3c23e63
Saksham Mittal <saksham1991999@users.noreply.github.com>
2021-02-20 00:15:03 +0530

    changed the translator

--- utilities/translator.py
+++ utilities/translator.py
@@ -1,13 +1,27 @@
 from googletrans import Translator, LANGUAGES
+from translate import Translator
+from google_trans_new import google_translator
 
 
-translator = Translator()
-languages = LANGUAGES
+#
+# languages = LANGUAGES
 
 
 def detect_comment(comment):
+    translator = Translator()
     return translator.detect(comment).lang
 
 
+# def translate_comment(comment, dest):
+#     return translator.translate(comment, dest=dest).text
+
+
+# def translate_comment(comment, dest):
+#     translator = Translator(to_lang=dest)
+#     translation = translator.translate(comment)
+#     return translation
+
 def translate_comment(comment, dest):
-    return translator.translate(comment, dest=dest).text
+    translator = google_translator()
+    translate_text = translator.translate(comment,lang_tgt=dest)
+    return translate_text
