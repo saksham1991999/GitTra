d4412a4339cd2db2c0c7560d5a5b018047130e71
Grace Gao <ggaoww@gmail.com>
2021-02-25 01:14:17 -0500

    restructured project and implemented click functions

--- gittra/commands.py
+++ gittra/commands.py
@@ -5,6 +5,7 @@ import os
 from gittra import script
 from gittra.utilities.git.gitcommands import *
 
+
 @click.group()
 def cli():
     pass
@@ -13,13 +14,9 @@ def cli():
 @cli.command()
 @click.argument("repo", type=str)
 @click.argument("lang", type=str)
-@click.option("-n", "--rename", type=str, help="Specify the name of the directory to clone into")
-def clone(repo, lang, rename, name="clone"):
-    """Clones a Github repository and translates all comments"""
-    if rename:
-        gitclone(repo, lang, rename)
-    else:
-        gitclone(repo, lang, "translated")
+def clone(repo, lang, name="clone"):
+    """Clones a Github repository and creates a translated branch"""
+    gittra_clone(repo, lang)
 
 
 @cli.command()
@@ -27,11 +24,10 @@ def clone(repo, lang, rename, name="clone"):
 @click.argument("lang", type=str)
 def fork(repo, lang, name="fork"):
     """Forks a Github repository and creates a translated branch"""
-    gitfork(repo, lang)
+    gittra_fork(repo, lang)
 
 
 @cli.command()
-@click.argument("lang", type=str)
 def push(name="push"):
     """Pushes into the translated branch of the Github fork"""
-    gitpush()
+    gittra_push()
--- gittra/utilities/git/gitcommit.py
+++
@@ -1,9 +0,0 @@
-#make commits to the origin git repository
-import sys
-import os
-import shutil
-import translator
-
-#make add all files
-
-#make a commit 
